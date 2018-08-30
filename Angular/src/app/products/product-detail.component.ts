96d6837c1988365bc9b0d818ed79fc51e17902bb
Karan Sharma <cserkaran@gmail.com>
2018-08-30 20:00:27 +1000

    Adding angular setup.

---
+++ Angular/src/app/products/product-detail.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductService } from './product.service';
+
+@Component({
+  // selector: 'pm-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
+  errorMessage: string;
+
+  constructor(private _route: ActivatedRoute, private _router: Router, private _productService: ProductService) {
+
+  }
+
+  ngOnInit() {
+    const param = this._route.snapshot.paramMap.get('id');
+    if (param) {
+      const id = +param;
+      this.getProduct(id);
+    }
+
+  }
+
+  onBack(){
+    this._router.navigate(['/products']);
+  }
+
+  getProduct(id: number) {
+    this._productService.getProduct(id).subscribe(
+      product => this.product = product,
+      error => this.errorMessage = <any>error);
+  }
+
+}

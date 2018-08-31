fbe73d163f7551b142c9a9245855f273015232c2
Karan Sharma <karan.sharma@praemium.com>
2018-08-31 14:05:44 +1000

    Cleaned up not required  stuff from angular setup

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -5,24 +5,17 @@ import {HttpClientModule} from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './home/welcome.component';
-
-import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WelcomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path:'welcome', component:WelcomeComponent},
-      { path:'', redirectTo:'welcome',pathMatch:'full'},
-      { path:'**', redirectTo:'welcome',pathMatch:'full'}
-    ]),
-    ProductModule
+      { path: '', component : AppComponent},
+    ])
   ],
   bootstrap: [AppComponent]
 })
